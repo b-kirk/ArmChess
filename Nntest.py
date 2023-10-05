94e35f743814ea939d18a62785685ad2ee8df386
bk <146019578+b-kirk@users.noreply.github.com>
2023-10-05 14:46:46 +0100

    Added documentation to CNN, added loss function and training

--- Nntest.py
+++ Nntest.py
@@ -64,6 +64,47 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3 , 6, 5) # 3 Input channels, 6 output channels, 5x5 convolution square
         self.pool = nn.MaxPool2d(2, 2) # A pool of the maximum values of each convolution (2x2 values), moving to the next two columns (stride undefined, uses kernel size) and repeating, thus halving the number of values.
         self.conv2 = nn.Conv2d(6, 16, 5) # 6 Input channels, 16 output channels, 5x5 convolution square again.
-        self.fc1 = nn.Linear(16*5*5,120) # Linear transform 
-        self.fc2 = nn.Linear(120, 84) # 
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(16*5*5,120) # Takes in_features (size of input sample) , out_features (size of output sample), & generates linear.weight equal to a parameter
+        self.fc2 = nn.Linear(120, 84) # Linear Map
+        self.fc3 = nn.Linear(84, 10) # Linear Map
+
+    def forward(self, x): # Init forward function
+        x = self.pool(F.relu(self.conv1(x))) 
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
