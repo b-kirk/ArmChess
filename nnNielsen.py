2d8996fd15d5161112eb0bfeb22bedfd2bcab184
bk <146019578+b-kirk@users.noreply.github.com>
2023-10-13 00:08:09 +0100

    Nielsen

--- nnNielsen.py
+++ nnNielsen.py
@@ -1,5 +1,9 @@
 import numpy as np
+import random
 import  PIL 
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z)) # 1.0 Used to specify float
 
 class net(object):
     '''
@@ -14,17 +18,33 @@ class net(object):
         self.biases = [np.random.randn(y,1) for y in neuronAmount[1:]]
         self.weights = [np.random.randn(y,x) for x,y in zip(neuronAmount[:-1], neuronAmount[1:])]
     
-    def sigmoid(z):
-        return 1.0/(1.0+np.exp(-z)) # 1.0 Used to specify float
     
     def feedForward(self, a):
         for b, w in zip(self.biases, self.weights):
             a = net.sigmoid(np.dot(w,a)+b)
-            
+            return a
+    def SGD(self, trainData, epochs, miniBatchSize , eta, testData = None):
+        if testData: testDataLength = len(testData)
+        n = len(trainData)
+        for j in range(epochs):
+            random.shuffle(trainData)
+            miniBatches = [ trainData[k:k+miniBatchSize] for k in range(0, n, miniBatchSize)]
+            for miniBatch in miniBatches:
+                self.updateMiniBatch(miniBatch, eta)
+            if testData:
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(testData), testDataLength))
+            else:
+                print("Epoch {0} complete".format(j))
+    def updateMiniBatch(self, miniBatch, eta):
+        """Apply gradient descent via backprop to a mini-batch (a list of tuples, "x, y"). Eta is the learning rate """
+        delB = [np.zeros(b.shape) for b in self.biases]
+        delW = [np.zeros(w.shape) for w in self.weights]
+        for x, y in miniBatch:
+            deltaDelB, deltaDelW = self.backProp(x, y)
+            delB = [db+ddb for db, ddb in zip(delB, deltaDelB)]
+            delW = [nw+dnw for nw, dnw in zip(delW, deltaDelW)]
+        self.weights = [w-(eta/len(miniBatch))*nw for w, nw in zip(self.weights, delW)]
+        self.biases = [b-(eta/len(miniBatch))*nb for b, nb in zip(self.biases, delB)]
 
 
-arr = [1,2,3,4,5]
 
-
-for x,y in zip(net.biases, net.weights):
-    print(np.random.randn(y,x))
